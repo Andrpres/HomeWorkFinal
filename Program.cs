6b382ee130228f33f598547cefe547b27c7a4680
Andrpre <andrpre96@gmail.com>
2023-01-07 08:02:30 +0300

    Выводим массив на экран

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿//Задаем первичный массив
 string[] mass1 = { "Russia", "Pol", "1478" };
+
+//Выводим массив на экран
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
