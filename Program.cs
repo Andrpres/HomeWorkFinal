2629b441fa3b0fe862dcc06b67646687f173f365
Andrpre <andrpre96@gmail.com>
2023-01-07 08:05:27 +0300

    Формируем новый массив, в котором оставляем строки в 3 или меньше символа

--- Program.cs
+++ Program.cs
@@ -9,3 +9,16 @@ void PrintArray(string[] array)
         Console.Write($"{array[i]} ");
     }
 }
+
+//Формируем новый массив, в котором оставляем строки в 3 или меньше символа
+string[] resultMass(string[] array){
+    string[] mass2 = new string[3];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3){
+           mass2[i] = array[i];
+        }
+    }
+    mass2 = mass2.Where(x => x != null).ToArray();
+    return mass2;
+}
